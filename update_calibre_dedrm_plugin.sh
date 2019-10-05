4bd58de1a2f19ed78b4e58717c10d446af9d6877
Cory Solovewicz <cory@solovewicz.com>
2019-10-04 19:44:31 -0700

    Update update_calibre_dedrm_plugin.sh
    Added some error checks and made sure files exist before unzipping, downloading, etc.

--- update_calibre_dedrm_plugin.sh
+++ update_calibre_dedrm_plugin.sh
@@ -27,12 +27,43 @@ fi
 # Download Alf's DeDRM tools
 printf "\n############################# Downloading AlfDeDRM Tools......... #############################"
 printf "\n"
-wget https://github.com/apprenticeharper/DeDRM_tools/releases/download/v$VERSION/DeDRM_tools_$VERSION.zip
+URL=https://github.com/apprenticeharper/DeDRM_tools/releases/download/v$VERSION/DeDRM_tools_$VERSION.zip
+# echo $URL
+# wget_output will be 0 if $URL is found, otherwise it is not found so we should exit
+
+if [ ! -e DeDRM_tools_$VERSION.zip ]
+then
+	wget_output=$(wget -q "$URL")
+	# check the value of wget_output (last command run)
+	if [ $? -eq 0 ]; 
+		then
+			echo "That version does exist. Downloading...."
+		else
+			echo "ERROR: Version $VERSION doesn't exist! Exiting...."
+			exit;
+	fi
+else
+	echo "DeDRM_tools_$VERSION.zip already exists, no need to re-download"
+fi
 
 # unzip DeDRM Tools
 printf "\n############################# Unzipping Alf's DeDRM Tools #############################"
 printf "\n"
-unzip DeDRM_tools_$VERSION.zip -d DeDRM_tools_$VERSION
+# Check if archive exists before attempting to unzip
+if [ -e DeDRM_tools_$VERSION.zip ]
+then
+	echo "DeDRM_tools_$VERSION.zip exists prepare to unzip"
+	if [ ! -e DeDRM_tools_$VERSION ]
+	then
+		unzip DeDRM_tools_$VERSION.zip -d DeDRM_tools_$VERSION
+	else
+		echo "The directory DeDRM_tools_$VERSION already exists, no need to unzip archive"
+	fi
+else
+	echo "ERROR: Something went wrong in downloading DeDRM_tools_$VERSION. Exiting...."
+	exit;
+fi
+
 
 # enter into calibre plugin directory
 cd DeDRM_tools_$VERSION/DeDRM_calibre_plugin/
@@ -40,7 +71,21 @@ cd DeDRM_tools_$VERSION/DeDRM_calibre_plugin/
 # unzip DeDRM_plugin.zip archive
 printf "\n############################# Unzipping Alf's DeDRM Calibre Plugin #############################"
 printf "\n"
-unzip DeDRM_plugin.zip -d DeDRM_plugin 
+# Check if archive exists before attempting to unzip
+if [ -e DeDRM_plugin.zip ]
+then
+	echo "DeDRM_plugin.zip exists prepare to unzip"
+	if [ ! -e DeDRM_plugin ]
+	then
+		unzip DeDRM_plugin.zip -d DeDRM_plugin 
+	else
+		echo "The directory DeDRM_plugin already exists, no need to unzip archive"
+	fi
+else
+	echo "ERROR: Something is wrong with DeDRM_tools_$VERSION. There is no DeDRM_plugin.zip. Exiting...."
+	exit;
+fi
+
 
 # enter unzip calibre plugin
 cd DeDRM_plugin
@@ -48,13 +93,25 @@ cd DeDRM_plugin
 # edit mobiderm.py to remove the library/rented book restriction
 printf "\n############################# Editing the mobiderm.py file to remove the library/rented ebooks restriction #############################"
 printf "\n"
-sed -i -e 's/if val406 != 0:/#if val406 != 0:/g' mobidedrm.py
-sed -i -e 's/raise DrmException(u"Cannot decode library or rented ebooks.")/#raise DrmException(u"Cannot decode library or rented ebooks.")/g' mobidedrm.py 
+if [ -e mobidedrm.py ]
+then
+	echo "mobidedrm.py exists preparing to edit"
+	sed -i -e 's/if val406 != 0:/#if val406 != 0:/g' mobidedrm.py
+	sed -i -e 's/raise DrmException(u"Cannot decode library or rented ebooks.")/#raise DrmException(u"Cannot decode library or rented ebooks.")/g' mobidedrm.py
+else
+	echo "ERROR: Something is wrong with DeDRM_tools_$VERSION. There is no mobidedrm.py. Exiting...."
+	exit;
+fi 
 
 # zip newly modified calbre plugin
 printf "\n############################# Zipping newly modified calbre plugin #############################"
 printf "\n"
 zip -r DeDRM_plugin_$VERSION.zip *
+if [ ! -e DeDRM_plugin_$VERSION.zip ]
+then
+	echo "ERROR: DeDRM_plugin_$VERSION.zip doesn't exist. Something went wrong when creating it. Exiting...."
+	exit;
+fi
 
 # set PATH environment variable if not already set
 printf "\n############################# Setting environment path to calibre commandline tools #############################"
@@ -94,4 +151,3 @@ printf "\nCalibre -> Preferences... -> Plugins -> File type plugins"
 printf "\nWhere you should see DeDRM($VERSION)"
 printf "\n"
 printf "\n"
-
